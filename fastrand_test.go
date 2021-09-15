679a6fad9d7a92a49002b0abd8f8ce28b5600f55
Aliaksandr Valialkin <valyala@gmail.com>
2021-09-15 08:58:00 +0300

    Add RNG.Seed function, which can be used for resetting the state of RNG to the given seed

--- fastrand_test.go
+++ fastrand_test.go
@@ -4,6 +4,21 @@ import (
 	"testing"
 )
 
+func TestRNGSeed(t *testing.T) {
+	var r RNG
+	for _, seed := range []uint32{1, 2, 1234, 432432, 34324432} {
+		r.Seed(seed)
+		m := make(map[uint32]struct{})
+		for i := 0; i < 1e6; i++ {
+			n := r.Uint32()
+			if _, ok := m[n]; ok {
+				t.Fatalf("number %v already exists", n)
+			}
+			m[n] = struct{}{}
+		}
+	}
+}
+
 func TestUint32(t *testing.T) {
 	m := make(map[uint32]struct{})
 	for i := 0; i < 1e6; i++ {
